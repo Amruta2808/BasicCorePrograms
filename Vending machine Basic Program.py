b7f2fa6dfa7ab98669720d63ddc3428bba63f856
Amruta Rajput <92590114+Amruta2808@users.noreply.github.com>
2021-10-19 22:14:45 +0530

    Added: Vending machine Basic Program

---
+++ Vending machine Basic Program.py
@@ -0,0 +1,12 @@
+y= int(input("?"))
+av=4
+i=1
+while i<=y:
+    if i>av:
+        print("Hey!! sorry out of stock")
+        break
+    else:
+        print("Candy")
+        i+=1
+        
+print("Have a nice day!!")    
