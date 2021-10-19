7f2061af2e372125613e7bf74585c35c1b161f02
Amruta Rajput <92590114+Amruta2808@users.noreply.github.com>
2021-10-19 22:16:42 +0530

    Added: Pattern code

---
+++ Pattern.py
@@ -0,0 +1,28 @@
+
+
+for i in range(4,0,-1):
+    while i>=1:
+        print("#", end='')
+        i-=1
+    print()
+
+
+for i in range(4):
+    for j in range(i+1):
+        print("*",end='')
+    print()
+
+
+for i in range(4):
+    for j in range(4-i):
+        print("*",end='')
+    print()
+
+
+print()
+print("Hey")
+
+        
+  
+
+
